f8e0fc847f547be1b3fe45237b6b49bac5e1eb0c
Muhammad Sultan <muhammad.sultan96@gmail.com>
2019-08-07 15:39:13 -0400

    Setup home, resume pages

--- src/app/app-routing/route-animations.ts
+++ src/app/app-routing/route-animations.ts
@@ -9,35 +9,89 @@ import {
 
 export const Slider =
   trigger('routeAnimations', [
-    transition('* => isLeft', slideTo('left') ),
-    transition('* => isRight', slideTo('right') ),
-    transition('isRight => *', slideTo('left') ),
-    transition('isLeft => *', slideTo('right') )
-  ]);
-
-function slideTo(direction) {
-  const optional = {optional: true};
-  return [
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 62,
-        [direction]: 0,
-        width: '100%'
-      })
-    ], optional),
-    query(':enter', [
-      style({[direction]: '-100%'})
-    ]),
-    group([
-      query(':leave', [
-        animate('300ms ease',
-          style({[direction]: '100%'}))
-      ], optional),
-      query(':enter', [
-        animate('300ms ease',
-          style({ [direction]: '0%'}))
+      transition('* => isLeft', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ left: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('600ms ease', style({ left: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('600ms ease', style({ left: '0%'}))
+          ])
+        ]),
+      ]),
+      transition('* => isRight', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ right: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('600ms ease', style({ right: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('600ms ease', style({ right: '0%'}))
+          ])
+        ]),
+      ]),
+      transition('isRight => *', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ left: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('600ms ease', style({ left: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('600ms ease', style({ left: '0%'}))
+          ])
+        ]),
+      ]),
+      transition('isLeft => *', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ right: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('600ms ease', style({ right: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('600ms ease', style({ right: '0%'}))
+          ])
+        ]),
       ])
-    ])
-  ];
-}
+    ],
+  )
